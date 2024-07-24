7432f7c53d224c55d24022b478687090fd1104ea
Hanka8 <hanka.maruskevicova@gmail.com>
2024-07-24 14:54:38 +0200

    multiplier update

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const scoreElement = document.getElementById('score');
-const livesElement = document.getElementById('lives');
-const wolfElement = document.getElementById('wolf');
-const gameContainerElement = document.querySelector('.game-container');
-const gameBoardElement = document.getElementById('gameBoard');
-const livesBoxElement = document.getElementById('livesBox');
-const modalElement = document.getElementById('modal');
+const scoreElement = document.getElementById("score");
+const livesElement = document.getElementById("lives");
+const wolfElement = document.getElementById("wolf");
+const gameContainerElement = document.querySelector(".game-container");
+const gameBoardElement = document.getElementById("gameBoard");
+const livesBoxElement = document.getElementById("livesBox");
+const modalElement = document.getElementById("modal");
 
-const startBtn1 = document.getElementById('startBtn1');
-const startBtn2 = document.getElementById('startBtn2');
+const startBtn1 = document.getElementById("startBtn1");
+const startBtn2 = document.getElementById("startBtn2");
 
-const leftTopBtn = document.getElementById('leftTopBtn');
-const leftBottomBtn = document.getElementById('leftBottomBtn');
-const rightTopBtn = document.getElementById('rightTopBtn');
-const rightBottomBtn = document.getElementById('rightBottomBtn');
-const closeModalBtn = document.getElementById('closeModalBtn');
+const leftTopBtn = document.getElementById("leftTopBtn");
+const leftBottomBtn = document.getElementById("leftBottomBtn");
+const rightTopBtn = document.getElementById("rightTopBtn");
+const rightBottomBtn = document.getElementById("rightBottomBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
 
 const EGG_FREQUENCY_CHANGE_A = 450;
 const EGG_SPEED_CHANGE_A = 40;
@@ -38,30 +38,30 @@ class Game {
   }
 
   start() {
-    document.addEventListener('keydown', this.handleKeyDown);
+    document.addEventListener("keydown", this.handleKeyDown);
 
-    leftBottomBtn.addEventListener('click', () => {
+    leftBottomBtn.addEventListener("click", () => {
       game.wolf.moveLeft();
       game.wolf.moveDown();
     });
-    leftTopBtn.addEventListener('click', () => {
+    leftTopBtn.addEventListener("click", () => {
       game.wolf.moveLeft();
       game.wolf.moveUp();
     });
-    rightBottomBtn.addEventListener('click', () => {
+    rightBottomBtn.addEventListener("click", () => {
       game.wolf.moveRight();
       game.wolf.moveDown();
     });
-    rightTopBtn.addEventListener('click', () => {
+    rightTopBtn.addEventListener("click", () => {
       game.wolf.moveRight();
       game.wolf.moveUp();
     });
 
     this.intervalId = setInterval(() => {
-      if (this.isEnded) { 
+      if (this.isEnded) {
         clearInterval(this.intervalId);
-        document.querySelectorAll('.egg').forEach(egg => {
-          egg.style.animationPlayState = 'paused';
+        document.querySelectorAll(".egg").forEach((egg) => {
+          egg.style.animationPlayState = "paused";
           gameBoardElement.removeChild(egg);
         });
         return;
@@ -78,12 +78,16 @@ class Game {
       //remove lifes when the score is at some point
       if (this.score == 200 || this.score == 500 || this.score == 1000) {
         this.lives = 0;
-        document.querySelectorAll('.life').forEach(lifeElement => {
+        document.querySelectorAll(".life").forEach((lifeElement) => {
           livesBoxElement.removeChild(lifeElement);
         });
       }
       //speeding up the game
-      if (this.score % 10 == 0 && this.eggFrequency >= 1000 && this.eggSpeed >= 500 ) {
+      if (
+        this.score % 10 == 0 &&
+        this.eggFrequency >= 1000 &&
+        this.eggSpeed >= 500
+      ) {
         if (this.gameType == "A") {
           this.eggFrequency -= EGG_FREQUENCY_CHANGE_A;
           this.eggSpeed -= EGG_SPEED_CHANGE_A;
@@ -91,31 +95,34 @@ class Game {
           this.eggFrequency -= EGG_FREQUENCY_CHANGE_B;
           this.eggSpeed -= EGG_SPEED_CHANGE_B;
         }
-      };
+      }
 
-      document.documentElement.style.setProperty("--animationDurationEgg", `${this.eggSpeed*4.1}ms`);
+      document.documentElement.style.setProperty(
+        "--animationDurationEgg",
+        `${this.eggSpeed * 4.1}ms`
+      );
     }, this.eggFrequency);
   }
 
   end() {
-    document.removeEventListener('keydown', this.handleKeyDown);
+    document.removeEventListener("keydown", this.handleKeyDown);
     this.isEnded = true;
-    let gameOverSound = new Audio('sounds/gameOver.mp3');
+    let gameOverSound = new Audio("sounds/gameOver.mp3");
     gameOverSound.play();
   }
 
   handleKeyDown(event) {
     switch (event.key) {
-      case 'ArrowLeft':
+      case "ArrowLeft":
         game.wolf.moveLeft();
         break;
-      case 'ArrowRight':
+      case "ArrowRight":
         game.wolf.moveRight();
         break;
-      case 'ArrowUp':
+      case "ArrowUp":
         game.wolf.moveUp();
         break;
-      case 'ArrowDown':
+      case "ArrowDown":
         game.wolf.moveDown();
         break;
       default:
@@ -126,18 +133,18 @@ class Game {
   updateScore() {
     this.score += 1;
     scoreElement.innerHTML = this.score;
-    let collectSound = new Audio('sounds/collect.mp3');
+    let collectSound = new Audio("sounds/collect.mp3");
     collectSound.play();
   }
 
   updateLives() {
     this.lives += 1;
     if (this.lives <= 3) {
-      let lifeSound = new Audio('sounds/lifeDown.mp3');
+      let lifeSound = new Audio("sounds/lifeDown.mp3");
       lifeSound.play();
-      let lifeElement = document.createElement('div');
-      lifeElement.classList.add('life');
-      livesBoxElement.appendChild(lifeElement); 
+      let lifeElement = document.createElement("div");
+      lifeElement.classList.add("life");
+      livesBoxElement.appendChild(lifeElement);
     }
     if (this.lives === 3) {
       this.end();
@@ -146,12 +153,12 @@ class Game {
 
   chickenRun(state) {
     let side = state.split("-")[1];
-    let chicken = document.createElement('div');
-    chicken.classList.add('chicken-running');
+    let chicken = document.createElement("div");
+    chicken.classList.add("chicken-running");
     if (side === "left") {
-      chicken.classList.add('running-left');
+      chicken.classList.add("running-left");
     } else {
-      chicken.classList.add('running-right');
+      chicken.classList.add("running-right");
     }
     gameBoardElement.appendChild(chicken);
     setTimeout(() => {
@@ -159,7 +166,6 @@ class Game {
     }, 2000);
   }
 }
-
 
 class Wolf {
   constructor() {
@@ -172,50 +178,56 @@ class Wolf {
 
   moveLeft() {
     if (this.up) {
-        wolfElement.style.backgroundImage = "url('assets/wolf_LeftUp.svg')";
-        this.state = "top-left"; 
+      wolfElement.style.backgroundImage = "url('assets/wolf_LeftUp.svg')";
+      this.state = "top-left";
     } else {
-        wolfElement.style.backgroundImage = "url('assets/wolf_LeftDown.svg')";
-        this.state = "bottom-left"; 
+      wolfElement.style.backgroundImage = "url('assets/wolf_LeftDown.svg')";
+      this.state = "bottom-left";
     }
-    document.documentElement.style.setProperty("--wolfPositionX", "calc(65px*var(--multiplier))");
+    document.documentElement.style.setProperty(
+      "--wolfPositionX",
+      "calc(65px*var(--multiplier))"
+    );
     this.right = false;
     this.left = true;
   }
 
   moveRight() {
     if (this.up) {
-        wolfElement.style.backgroundImage = "url('assets/wolf_RightUp.svg')";
-        this.state = "top-right"; 
-     } else {
-        wolfElement.style.backgroundImage = "url('assets/wolf_RightDown.svg')";
-        this.state = "bottom-right"; 
-     }
-    document.documentElement.style.setProperty("--wolfPositionX", "calc(125px*var(--multiplier))");
+      wolfElement.style.backgroundImage = "url('assets/wolf_RightUp.svg')";
+      this.state = "top-right";
+    } else {
+      wolfElement.style.backgroundImage = "url('assets/wolf_RightDown.svg')";
+      this.state = "bottom-right";
+    }
+    document.documentElement.style.setProperty(
+      "--wolfPositionX",
+      "calc(125px*var(--multiplier))"
+    );
     this.right = true;
     this.left = false;
   }
 
   moveUp() {
     if (this.left) {
-        wolfElement.style.backgroundImage = "url('assets/wolf_LeftUp.svg')";
-        this.state = "top-left"; 
-     } else {
-        wolfElement.style.backgroundImage = "url('assets/wolf_RightUp.svg')";
-        this.state = "top-right";
-     }
+      wolfElement.style.backgroundImage = "url('assets/wolf_LeftUp.svg')";
+      this.state = "top-left";
+    } else {
+      wolfElement.style.backgroundImage = "url('assets/wolf_RightUp.svg')";
+      this.state = "top-right";
+    }
     this.up = true;
     this.down = false;
   }
 
   moveDown() {
     if (this.left) {
-        wolfElement.style.backgroundImage = "url('assets/wolf_LeftDown.svg')";
-        this.state = "bottom-left"; 
-     } else {
-        wolfElement.style.backgroundImage = "url('assets/wolf_RightDown.svg')";
-        this.state = "bottom-right";
-     }
+      wolfElement.style.backgroundImage = "url('assets/wolf_LeftDown.svg')";
+      this.state = "bottom-left";
+    } else {
+      wolfElement.style.backgroundImage = "url('assets/wolf_RightDown.svg')";
+      this.state = "bottom-right";
+    }
     this.up = false;
     this.down = true;
   }
@@ -230,31 +242,27 @@ class Wolf {
   }
 }
 
-
 class Egg {
-
   constructor() {
     let eggOptions = ["bottom-left", "bottom-right", "top-left", "top-right"];
     this.state = eggOptions[Math.floor(Math.random() * eggOptions.length)];
     // prevent eggs to slide directly after each other
     while (this.state === game.previousEggState) {
       this.state = eggOptions[Math.floor(Math.random() * eggOptions.length)];
-    };
+    }
     game.previousEggState = this.state;
     this.timeOfExistence = 0;
   }
 
-  fall() { 
-
-    let eggElement = document.createElement('div');
-    eggElement.classList.add('egg');
+  fall() {
+    let eggElement = document.createElement("div");
+    eggElement.classList.add("egg");
     eggElement.classList.add(`egg-${this.state}`);
     gameBoardElement.appendChild(eggElement);
 
     const self = this;
 
     const interval = setInterval(() => {
-
       self.timeOfExistence += 1;
       if (self.timeOfExistence === 4 && !game.isEnded) {
         clearInterval(interval);
@@ -264,14 +272,13 @@ class Egg {
 
       // let eggSound = new Audio('sounds/egg.mp3');
       // eggSound.play();
-    
-      }, game.eggSpeed);
+    }, game.eggSpeed);
   }
 
   crash() {
     if (!game.isEnded) {
-      let crashedEggElement = document.createElement('div');
-      crashedEggElement.classList.add('egg-crashed');
+      let crashedEggElement = document.createElement("div");
+      crashedEggElement.classList.add("egg-crashed");
       crashedEggElement.classList.add(`egg-crashed-${this.state}`);
       gameBoardElement.appendChild(crashedEggElement);
 
@@ -286,67 +293,85 @@ class Egg {
 
 const game = new Game();
 
-startBtn1.addEventListener('click', () => {
- if (game.isEnded) {
-  location.reload();
- }
- if (!game.gameStarted) {
-      game.start();
-      game.gameStarted = true;
-      game.gameType = "A";
-    }
-});
-
-startBtn2.addEventListener('click', () => {
+startBtn1.addEventListener("click", () => {
   if (game.isEnded) {
     location.reload();
   }
   if (!game.gameStarted) {
-        game.eggSpeed = 1200;
-        game.eggFrequency = 4000;
-        game.start();
-        game.gameStarted = true;
-        game.gameType = "B";
-        game.eggSpeed = 800;
-        game.eggFrequency = 4000;
-      }
+    game.start();
+    game.gameStarted = true;
+    game.gameType = "A";
+  }
 });
 
+startBtn2.addEventListener("click", () => {
+  if (game.isEnded) {
+    location.reload();
+  }
+  if (!game.gameStarted) {
+    game.eggSpeed = 1200;
+    game.eggFrequency = 4000;
+    game.start();
+    game.gameStarted = true;
+    game.gameType = "B";
+    game.eggSpeed = 800;
+    game.eggFrequency = 4000;
+  }
+});
+
+// RESPONSIBILITY
+
 function responsibility() {
-  const gameContainerWidth = gameContainerElement.offsetWidth;
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight;
+  const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
 
   const regex = /\((.*?)px/;
 
-  const containerHeightString = getComputedStyle(document.documentElement).getPropertyValue('--gameContainerHeight'); 
+  const containerHeightString = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--gameContainerHeight");
+  const containerWidthString = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--gameContainerWidth");
+
   const containerHeight = parseInt(containerHeightString.match(regex)[1]);
+  const containerWidth = parseInt(containerWidthString.match(regex)[1]);
 
-  const containerWidhtString = getComputedStyle(document.documentElement).getPropertyValue('--gameContainerWidth'); 
-  const containerWidth = parseInt(containerHeightString.match(regex)[1]);
+  const newMultiplierBasedOnHeight = Math.min(
+    windowHeight / containerHeight,
+    MAX_MULTIPLIER
+  );
+  const newMultiplierBasedOnWidth = Math.min(
+    windowWidth / containerWidth,
+    MAX_MULTIPLIER
+  );
 
-  const newMultiplierBasedOnHeight = (windowHeight/containerHeight) > MAX_MULTIPLIER ? MAX_MULTIPLIER : (windowHeight/containerHeight);
-  const newMultiplierBasedOnWidth = (windowWidth/containerWidth) > MAX_MULTIPLIER ? MAX_MULTIPLIER : (windowWidth/containerWidth);
+  const isNarrow = windowWidth / windowHeight < 1.6;
 
-   if (windowWidth < 2400) {
-    if (windowWidth / windowHeight < 1.6) {
-      document.documentElement.style.setProperty("--multiplier", `${newMultiplierBasedOnWidth/2}`);
-    } else {
-      document.documentElement.style.setProperty("--multiplier", `${newMultiplierBasedOnHeight}`);
-    }
-   } else {
-    document.documentElement.style.setProperty("--multiplier", "2.5");
-   }
+  let newMultiplier;
+  if (windowWidth < 2400) {
+    newMultiplier = isNarrow
+      ? newMultiplierBasedOnWidth / 1.1
+      : newMultiplierBasedOnHeight;
+  } else {
+    newMultiplier = MAX_MULTIPLIER;
+  }
 
-   console.log(document.documentElement.style.getPropertyValue('--multiplier'));
+  document.documentElement.style.setProperty(
+    "--multiplier",
+    `${newMultiplier}`
+  );
+  console.log(newMultiplier);
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", responsibility);
+window.addEventListener("load", responsibility);
+
+window.addEventListener("resize", () => {
   responsibility();
   pleaseRotate();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   responsibility();
   pleaseRotate();
 });
@@ -355,13 +380,16 @@ window.addEventListener('load', () => {
 
 function pleaseRotate() {
   if (window.screen.width <= 700) {
-    if (window.screen.orientation.type === "portrait-primary" || window.screen.orientation.type === "portrait-secondary") {
-        modalElement.classList.add('shown');
-      } else {
-        modalElement.classList.remove('shown');
-      }
-  } 
-  closeModalBtn.addEventListener('click', () => {
-    modalElement.classList.remove('shown');
+    if (
+      window.screen.orientation.type === "portrait-primary" ||
+      window.screen.orientation.type === "portrait-secondary"
+    ) {
+      modalElement.classList.add("shown");
+    } else {
+      modalElement.classList.remove("shown");
+    }
+  }
+  closeModalBtn.addEventListener("click", () => {
+    modalElement.classList.remove("shown");
   });
 }
